009d0a23f01f01aa342dbc603eeec0c8fe5a2947
Noyan Berkay Aşkın <74705281+Vitoleone@users.noreply.github.com>
2023-04-20 19:18:54 +0300

    Scriptable added
    Scriptable for holding default values of player attributes is added

---
+++ Assets/Scripts/Player/PlayerAttributesScriptable.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PlayerAttributes", fileName = "PlayerAttribute")]
+public class PlayerAttributesScriptable : ScriptableObject
+{
+   //Ammo attributes
+   public int maxAmmo;
+   public int currentAmmo;
+   public int ammoLevel;
+   
+   //Damage attributes
+   public int rocketDamage;
+   public int damageLevel;
+   
+   //Defensive attributes
+   public int maxHealth;
+   public int currentHealth;
+   public int healthLevel;
+   
+   //In-Game values
+   public int money;
+   public int gem;
+}
