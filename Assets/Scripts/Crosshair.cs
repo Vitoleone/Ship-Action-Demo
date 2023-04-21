c53f46eb19a8f7160b97e48a477869b4c94de81d
Noyan Berkay Aşkın <74705281+Vitoleone@users.noreply.github.com>
2023-04-21 19:55:19 +0300

    Crosshair changes
    When crosshair on enemy crosshair's color become red, normally grey.

---
+++ Assets/Scripts/Crosshair.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crosshair : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("Enemy"))
+        {
+            spriteRenderer.color = Color.red;
+        }
+        else
+        {
+            spriteRenderer.color = Color.grey;
+        }
+    }
+}
