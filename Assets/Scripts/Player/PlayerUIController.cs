25db7b6bfb8508f0c1381f085319f226d06c179d
Noyan Berkay Aşkın <74705281+Vitoleone@users.noreply.github.com>
2023-04-23 14:49:00 +0300

    open and close upgrade, fly panel methods added

--- Assets/Scripts/Player/PlayerUIController.cs
+++ Assets/Scripts/Player/PlayerUIController.cs
@@ -11,10 +11,16 @@ public class PlayerUIController : MonoBehaviour
     [SerializeField] private TextMeshProUGUI currentAmmoText;
     [SerializeField] private TextMeshProUGUI maxAmmoText;
     [SerializeField] private TextMeshProUGUI goldText;
+    [SerializeField] private GameObject shootButton;
+    [SerializeField] private GameObject moveJoyStick;
+    [SerializeField] private GameObject upgradePanel;
+    [SerializeField] private GameObject ammoPanel;
+    [SerializeField] private GameObject healthbarPanel;
+    [SerializeField] private GameObject flyButton;
 
-    public void SetHealthBar(int currentHealth, int maxHealth)
+    public void SetHealthBar(float currentHealth, float maxHealth)
     {
-        healthBar.fillAmount = ((float)currentHealth / (float)maxHealth);
+        healthBar.fillAmount = (currentHealth / maxHealth);
     }
 
     public void SetAmmoTextValues(int currentAmmo,int maxAmmo)
@@ -26,5 +32,32 @@ public class PlayerUIController : MonoBehaviour
     public void SetGoldTextValue(int money)
     {
         goldText.text = money.ToString();
+    }
+
+    public void OpenUpgradeMenu()
+    {
+        upgradePanel.SetActive(true);
+        moveJoyStick.SetActive(false);
+        shootButton.SetActive(false);
+        healthbarPanel.SetActive(false);
+        ammoPanel.SetActive(false);
+    }
+
+    public void OpenFlyButton()
+    {
+        flyButton.SetActive(true);
+    }
+
+    public void CloseFlyButton()
+    {
+        flyButton.SetActive(false);
+    }
+    public void CloseUpgradeMenu()
+    {
+        upgradePanel.SetActive(false);
+        moveJoyStick.SetActive(true);
+        shootButton.SetActive(true);
+        healthbarPanel.SetActive(true);
+        ammoPanel.SetActive(true);
     }
 }
