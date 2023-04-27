5b8797a66ee27134e045d4fee7bb1c251adf7174
Noyan Berkay Aşkın <74705281+Vitoleone@users.noreply.github.com>
2023-04-27 14:22:43 +0300

    Crosshair movement adjustment
    Crosshair now moving more smoothly than past.

--- Assets/Scripts/Crosshair.cs
+++ Assets/Scripts/Crosshair.cs
@@ -1,11 +1,22 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class Crosshair : MonoBehaviour
 {
     [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private GameObject player;
+    
+
+    private void Update()
+    {
+        gameObject.transform.position =
+            new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+        gameObject.transform.Rotate(new Vector3(0,0,player.transform.rotation.z));
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.CompareTag("Enemy"))
@@ -17,4 +28,6 @@ public class Crosshair : MonoBehaviour
             spriteRenderer.color = Color.green;
         }
     }
+
+    
 }
